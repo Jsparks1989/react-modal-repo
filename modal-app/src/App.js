b37c87f828d3c7aaffd6a715231a98b857009fa4
Jsparks1989 <jsparks19891@gmail.com>
2019-11-20 16:06:05 -0500

    Provided the minimal core code required to have a functional modal window that opens and closes.

--- modal-app/src/App.js
+++ modal-app/src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {Modal, Button} from 'react-bootstrap';
 
+/*
+ * 
+ * 1) The state variable 'show' is used to determine the visibility of the modal window. 
+ *    state.show is set to false by default. it can be changed by using one of the 2 functions,
+ *    'handleClose' or 'handleShow'.
+ * 
+ * 2) The <Button>Click Me!</Button> element has an onClick attribute that will set state.show to true,
+ *    triggering the Modal window to open. The <Modal> element has a show attribute set to the state.show, 
+ *    this is how the Modal window knows when to open. 
+ * 
+ * 3) The <Modal> element also has a onHide attribute set to 'handleClose'. Whenever the handleClose function is ran,
+ *    the modal window knows to close. The two buttons in Modal.Footer have onClick attributes set to handleClose. When
+ *    these buttons are pressed, the modal window will close.
+ * 
+ */
 function App() {
+
+  // state variable
+  const [show, setShow] = useState(false);
+
+
+  // Functions that can change the state variable
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* the Button that opens the modal window */}
+      <Button variant="primary" onClick={handleShow}>
+        Click Me!
+      </Button>
+
+
+      {/* MODAL WINDOW */}
+      <Modal show={show} onHide={handleClose}>
+        {/* Modal Window Heading */}
+        <Modal.Header closeButton>
+          <Modal.Title>Modal Heading</Modal.Title>
+        </Modal.Header>
+
+        {/* Modal Window Body */}
+        <Modal.Body>This is the body of the modal window</Modal.Body>
+
+        {/* Modal Window Footer */}
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>Close</Button>
+          <Button variant="primary" onClick={handleClose}>Save</Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
 
 export default App;
+
